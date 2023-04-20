c0a12071d347ea74e931c68af043c92bb123fa1e
Sohan Kamble <sushantkumar.sk111@gmail.com>
2023-04-20 19:01:21 +0530

    Test

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,17 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
+import { useEffect } from 'react'
+import { useRouter } from 'next/router';
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
+  const router = useRouter();
+  useEffect(()=> {
+      setTimeout(() => {
+        router.push("https://websitewind.com")
+      }, 4000)
+  }, []);
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
